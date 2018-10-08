34959040ce7d948eb356d2a6addb4e89d67f4e26
Vincent Chho <vincec362@gmail.com>
2018-10-08 12:38:01 -0700

    Created JWT and added secret

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  'mongoURI': `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@ds147079.mlab.com:47079/devconnector`
+  'mongoURI': `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASS}@ds147079.mlab.com:47079/devconnector`,
+  'secret': 'supersecret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User Model
 const User = require('../../models/User');
@@ -58,14 +60,23 @@ router.post('/login', (req, res) => {
         .then(user => {
             // Check for user
             if(!user){
-                return res.status(404).json({email: 'User email not found'});
+                return res.status(404).json({email: 'User not found'});
             }
 
             // Check password
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch){
-                        res.json({msg: 'Success'});
+                        // User matched
+                        const payload = { id: user.id, name: user.name, avatar: user.avatar } //Creating JWT Payload
+
+                        // Sign Token
+                        jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token
+                            });
+                        });
                     } else {
                         return res.status(400).json({password: 'Password incorrect'});
                     }
