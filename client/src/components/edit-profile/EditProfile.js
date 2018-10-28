5a3e9b2ae494dbf133bb5209a4d0acf5f6d65294
Vincent Chho <vincec362@gmail.com>
2018-10-27 20:46:33 -0700

    (feat): Dashbaord Education display and delete

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -6,7 +6,7 @@ import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import { createProfile, getCurrentProfile } from "../../actions/profileActions";
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import isEmpty from '../../validation/is-empty';
 
 
@@ -174,6 +174,9 @@ class CreateProfile extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
+                        <Link to="/dashboard" className="btn btn-light">
+                            Go Back
+                        </Link>
                             <h1 className="display-4 text-center">Edit Profile</h1>
                             <small className="d-block pb-3">* = requried fields </small>
                             <form onSubmit={this.onSubmit}>
