e7c5b3564a153bd6811b7a56832c458de1f3342a
Vincent Chho <vincec362@gmail.com>
2018-10-30 00:02:24 -0700

    (style): changed date format for Moment

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -16,7 +16,7 @@ class Education extends Component {
                 <td>{edu.school}</td>
                 <td>{edu.degree}</td>
                 <td>
-                    <Moment format="YYYY/MM/DD">{edu.from}</Moment> - {edu.to === null ? (' Now') : (<Moment format="YYYY/MM/DD">{edu.to}</Moment>)}
+                    <Moment format="MM/DD/YYYY">{edu.from}</Moment> - {edu.to === null ? (' Now') : (<Moment format="MM/DD/YYYY">{edu.to}</Moment>)}
                 </td>
                 <td>
                     <button onClick={this.onDeleteClick.bind(this, edu._id)} className="btn btn-danger">Delete</button>
