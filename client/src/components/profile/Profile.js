ad300377f4370b0e2dcfeb1e7ec0517f67effcea
Vincent Chho <vincec362@gmail.com>
2018-10-30 00:18:49 -0700

    (style): changed Link to a tag

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -57,9 +57,7 @@ class Profile extends Component {
             <div className="profile">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-12">
-                            {profileContent}
-                        </div>
+                        <div className="col-md-12">{profileContent}</div>
                     </div>
                 </div>
             </div>
